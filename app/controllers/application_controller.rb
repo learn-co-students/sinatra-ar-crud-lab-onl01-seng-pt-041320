01eab7a1cea74a517ca8ed6cc1e1d194900ca7d8
snganyon@terpmail.umd.edu <snganyone1@gmail.com>
2020-07-16 20:47:28 -0700

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,25 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do
+  #Read 
+
+  get '/articles' do
+    @article = Article.all
+    erb :index
+  end
+
+  get '/articles/:id' do 
+    @article = Article.find(params[:id])
+    erb :show
+  end
+
+  #Create
+
+  get '/articles/new' do 
+    erb :new
+  end
+
+  post '/articles' do 
+    
   end
 end
