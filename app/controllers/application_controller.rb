9ddbbc5421aacb78736e46804606cda48dcf8072
github usernameIfeAkiWad <github email addressifeoluwa.wade@gmail.com>
2020-07-19 23:16:10 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,3 @@
-
 require_relative '../../config/environment'
 
 class ApplicationController < Sinatra::Base
@@ -9,5 +8,58 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
+  
   end
+
+  get '/articles' do
+    @articles = Article.all 
+    erb :index
+  end
+
+  get '/articles/new' do 
+
+    erb :new
+   
+  end
+
+  get '/articles/:id' do 
+
+    @article = Article.find(params[:id])
+
+    erb :show
+  end
+
+  
+  post '/articles' do 
+
+    @article = Article.create(params[:article])
+
+    redirect to "/articles/#{ @article.id }"
+
+    erb :show
+  end
+
+  get '/articles/:id/edit' do 
+    @article = Article.find_by_id(params[:id])
+
+    erb :edit
+  end
+
+  patch '/articles/:id' do 
+    article = Article.find_by_id(params[:id])
+    article.update(params[:article])
+ 
+    redirect "/articles/#{ @article.id }"
+  end
+
+  delete '/articles/:id' do
+    @article = Article.find_by_id(params[:id])
+    @article.destroy
+
+    redirect '/articles'
+
+    erb :delete
+  end
+
+    
 end
